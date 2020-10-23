1793c349bfab6311016aa36b9874a31c51551d60
NoSkillGirl <singhpooja240393@gmail.com>
2020-10-24 01:01:35 +0530

    fixed no-kind-pod error

--- pkg/client/clientset/versioned/scheme/register.go
+++ pkg/client/clientset/versioned/scheme/register.go
@@ -27,6 +27,7 @@ import (
 	serializer "k8s.io/apimachinery/pkg/runtime/serializer"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 )
 
 var Scheme = runtime.NewScheme()
@@ -36,6 +37,7 @@ var localSchemeBuilder = runtime.SchemeBuilder{
 	kyvernov1.AddToScheme,
 	policyv1alpha1.AddToScheme,
 	appsv1.AddToScheme,
+	corev1.AddToScheme,
 }
 
 // AddToScheme adds all types of this clientset into the given scheme. This allows composition
