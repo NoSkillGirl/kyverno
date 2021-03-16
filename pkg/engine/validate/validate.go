1cf25ca8649d01e67827f5df536ad7cfca7746c0
NoSkillGirl <singhpooja240393@gmail.com>
2021-03-16 09:32:47 +0530

    temp
    Signed-off-by: NoSkillGirl <singhpooja240393@gmail.com>

--- pkg/engine/validate/validate.go
+++ pkg/engine/validate/validate.go
@@ -82,24 +82,194 @@ func validateResourceElement(log logr.Logger, resourceElement, patternElement, o
 	return "", nil
 }
 
+// func checkOrigPattern(origPattern interface{}) (string, error) {
+// 	fmt.Println("\n---------checkOrigPattern-------")
+// 	fmt.Println("origPattern: ", origPattern)
+// 	switch typedOrigPattern := origPattern.(type) {
+// 	case map[string]interface{}:
+// 		for _, typedOrigPatternElement := range typedOrigPattern {
+// 			checkOrigPattern(typedOrigPatternElement)
+// 		}
+// 	case []interface{}:
+// 		for _, typedOrigPatternElement := range typedOrigPattern {
+// 			checkOrigPattern(typedOrigPatternElement)
+// 		}
+// 	case string:
+// 		return typedOrigPattern, nil
+// 	default:
+// 		fmt.Println("default-------type:- %T!!!!\n", origPattern)
+// 	}
+// 	return "", nil
+// }
+
+// func seperateOriginalPattern(origPattern map[string]interface{}, key string) interface{} {
+// 	// var patterns map[string]interface{}
+// 	patternKey := make([]string, 0)
+// 	fmt.Println("\n---origPattern: ", origPattern)
+// 	fmt.Println("key: ", key)
+// 	for k, v := range origPattern {
+// 		fmt.Println("k: ", k)
+// 		fmt.Println("v: ", v)
+// 		if k != key {
+// 			typedV, ok := v.(map[string]interface{})
+// 			if !ok {
+// 				fmt.Println("1---typedV----type:- %T!!!!\n", typedV)
+// 				fmt.Println("checking for array of interface........")
+// 				typedV1, ok := v.([]interface{})
+// 				if !ok {
+// 					fmt.Println("2---typedV----type:- %T!!!!\n", typedV1)
+// 				}
+// 				for k1, v1 := range typedV1 {
+// 					fmt.Println("k1: ", k1)
+// 					fmt.Println("v1: ", v1)
+// 				}
+// 			}
+// 			// fmt.Println("before----patterns:  ", patterns)
+// 			// patterns[k] = seperateOriginalPattern(typedV, key)
+// 			// fmt.Println("after ----patterns:  ", patterns)
+// 			patternKey = append(patternKey, k)
+// 			fmt.Println("patternKey:  ", patternKey)
+// 		}
+// 	}
+// 	return "some stringggggggg"
+// }
+
+var patternKey []string
+
+func getOriginalPatternKey(origPattern interface{}, key string) []string {
+	fmt.Println("\n-------getOriginalPatternKey---------")
+	fmt.Println("origPattern: ", origPattern)
+	// patternKey := make([]string, 0)
+	switch typedOrigPattern := origPattern.(type) {
+	case map[string]interface{}:
+		fmt.Println("===map[string]interface{}")
+		for k, v := range typedOrigPattern {
+			patternKey = append(patternKey, k)
+			fmt.Println("patternKey: ", patternKey)
+			getOriginalPatternKey(v, key)
+		}
+	case []interface{}:
+		fmt.Println("===[]interface{}")
+		for _, v := range typedOrigPattern {
+			fmt.Println("v: ", v)
+			combineMap(v)
+		}
+	}
+	return nil
+}
+
+var combineOriginalPatternMap map[string]interface{}
+
+func combineMap(v interface{}) {
+	fmt.Println("#### v: ", v)
+	l := len(patternKey) - 1
+	for i := l; i >= 0; i-- {
+		temp := make(map[string]interface{})
+		fmt.Println("patternKey[i]: ", patternKey[i])
+		if i == l {
+			temp[patternKey[i]] = v
+			combineOriginalPatternMap = temp
+			fmt.Println("---temp: ", temp)
+			fmt.Println("---combineOriginalPatternMap: ", combineOriginalPatternMap)
+			continue
+		}
+		temp[patternKey[i]] = combineOriginalPatternMap
+		combineOriginalPatternMap = temp
+		fmt.Println("###temp: ", temp)
+		fmt.Println("###combineOriginalPatternMap: ", combineOriginalPatternMap)
+	}
+	fmt.Println("############combineOriginalPatternMap: ", combineOriginalPatternMap)
+}
+
 // If validateResourceElement detects map element inside resource and pattern trees, it goes to validateMap
 // For each element of the map we must detect the type again, so we pass these elements to validateResourceElement
 func validateMap(log logr.Logger, resourceMap, patternMap map[string]interface{}, origPattern interface{}, path string, ac *common.AnchorKey) (string, error) {
+	fmt.Println("\n---------validateMap-------")
+	fmt.Println("resourceMap: ", resourceMap)
+	fmt.Println("patternMap: ", patternMap)
+	fmt.Println("origPattern: ", origPattern)
 
+	// patternMap = wildcards.ExpandInMetadata(patternMap, resourceMap)
+	// // check if there is anchor in pattern
+	// // Phase 1 : Evaluate all the anchors
+	// // Phase 2 : Evaluate non-anchors
+	// anchors, resources := anchor.GetAnchorsResourcesFromMap(patternMap)
+
+	// origPatternSting, err := checkOrigPattern(origPattern)
+	// if err != nil {
+	// 	fmt.Println("-----------error -----here")
+	// }
+	// fmt.Println("origPatternSting: ", origPatternSting)
+
+	// // Evaluate anchors
+	// for key, patternElement := range anchors {
+
+	// 	// get handler for each pattern in the pattern
+	// 	// - Conditional
+	// 	// - Existence
+	// 	// - Equality
+	// 	handler := anchor.CreateElementHandler(key, patternElement, path)
+	// 	handlerPath, err := handler.Handle(validateResourceElement, resourceMap, origPatternSting, ac)
+	// 	// if there are resource values at same level, then anchor acts as conditional instead of a strict check
+	// 	// but if there are non then its a if then check
+	// 	if err != nil {
+	// 		// If Conditional anchor fails then we don't process the resources
+	// 		if commonAnchors.IsConditionAnchor(key) {
+	// 			ac.AnchorError = common.NewConditionalAnchorError(fmt.Sprintf("condition anchor did not satisfy: %s", err.Error()))
+	// 			log.V(3).Info(ac.AnchorError.Message)
+	// 			return "", ac.AnchorError.Error()
+	// 		}
+	// 		return handlerPath, err
+	// 	}
+	// }
+
+	// // Evaluate resources
+	// // getSortedNestedAnchorResource - keeps the anchor key to start of the list
+	// sortedResourceKeys := getSortedNestedAnchorResource(resources)
+	// for e := sortedResourceKeys.Front(); e != nil; e = e.Next() {
+	// 	key := e.Value.(string)
+	// 	handler := anchor.CreateElementHandler(key, resources[key], path)
+	// 	handlerPath, err := handler.Handle(validateResourceElement, resourceMap, origPatternSting, ac)
+	// 	if err != nil {
+	// 		return handlerPath, err
+	// 	}
+	// }
+
+	// return "", nil
+
+	// ----------------------------------------------------------------------------------
 	patternMap = wildcards.ExpandInMetadata(patternMap, resourceMap)
 	// check if there is anchor in pattern
 	// Phase 1 : Evaluate all the anchors
 	// Phase 2 : Evaluate non-anchors
 	anchors, resources := anchor.GetAnchorsResourcesFromMap(patternMap)
-
+	fmt.Println("anchors: ", anchors)
 	// Evaluate anchors
 	for key, patternElement := range anchors {
-
+		if commonAnchors.IsExistenceAnchor(key) {
+			fmt.Printf("type %T!\n", patternElement)
+			typedPatternElement, ok := patternElement.([]interface{})
+			if !ok {
+				fmt.Println("type %T!\n", typedPatternElement)
+			}
+			for _, v := range typedPatternElement {
+				fmt.Println("----typedPatternElement: ", v)
+			}
+			typedOrigPattern, ok := origPattern.(map[string]interface{})
+			if !ok {
+				fmt.Println("type %T!\n", typedOrigPattern)
+			}
+			a := getOriginalPatternKey(typedOrigPattern, key)
+			fmt.Println("aaaaaaaa: ", a)
+		}
 		// get handler for each pattern in the pattern
 		// - Conditional
 		// - Existence
 		// - Equality
 		handler := anchor.CreateElementHandler(key, patternElement, path)
+		fmt.Println("key: ", key)
+		fmt.Println("patternElement: ", patternElement)
+		fmt.Println("-----here---origPattern: ", origPattern)
 		handlerPath, err := handler.Handle(validateResourceElement, resourceMap, origPattern, ac)
 		// if there are resource values at same level, then anchor acts as conditional instead of a strict check
 		// but if there are non then its a if then check
@@ -126,6 +296,7 @@ func validateMap(log logr.Logger, resourceMap, patternMap map[string]interface{}
 		}
 	}
 	return "", nil
+
 }
 
 func validateArray(log logr.Logger, resourceArray, patternArray []interface{}, originPattern interface{}, path string, ac *common.AnchorKey) (string, error) {
